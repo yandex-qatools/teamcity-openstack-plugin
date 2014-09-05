e5bb1cfd760e25e04128c5bfe0fbc15a237628bd
mavlyutov <mavlyutov@yandex-team.ru>
2014-09-05 20:08:46 +0400

    do n ot decode base64, that will do CloudInstanceUserData for us

--- cloud-openstack-agent/src/main/java/jetbrains/buildServer/clouds/openstack/OpenstackAgentProperties.java
+++ cloud-openstack-agent/src/main/java/jetbrains/buildServer/clouds/openstack/OpenstackAgentProperties.java
@@ -10,13 +10,13 @@ import jetbrains.buildServer.clouds.CloudInstanceUserData;
 import jetbrains.buildServer.log.Loggers;
 import jetbrains.buildServer.util.EventDispatcher;
 import jetbrains.buildServer.util.StringUtil;
-import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 
@@ -40,30 +40,24 @@ public class OpenstackAgentProperties extends AgentLifeCycleAdapter {
             // fill metadata parameters
             String uuid = getParameterFromJson(metadata, "uuid");
             if (uuid != null) {
-                LOG.debug(String.format("Setting %s to %s", OpenstackCloudParameters.OPENSTACK_INSTANCE_ID, uuid));
+                LOG.info(String.format("Setting %s to %s", OpenstackCloudParameters.OPENSTACK_INSTANCE_ID, uuid));
                 agentConfiguration.addConfigurationParameter(OpenstackCloudParameters.OPENSTACK_INSTANCE_ID, uuid);
             }
 
             String name = getParameterFromJson(metadata, "name");
             if (name != null) {
-                LOG.debug(String.format("Setting name to %s", name));
+                LOG.info(String.format("Setting name to %s", name));
                 agentConfiguration.setName(name);
             }
 
-            LOG.debug(String.format("Setting %s to %s", OpenstackCloudParameters.AGENT_CLOUD_TYPE, OpenstackCloudParameters.CLOUD_TYPE));
+            LOG.info(String.format("Setting %s to %s", OpenstackCloudParameters.AGENT_CLOUD_TYPE, OpenstackCloudParameters.CLOUD_TYPE));
             agentConfiguration.addConfigurationParameter(OpenstackCloudParameters.AGENT_CLOUD_TYPE, OpenstackCloudParameters.CLOUD_TYPE);
 
             // fill userdata parameters
             String userData = readDataFromUrl(userDataUrl);
             if (!StringUtil.isEmpty(userData)) {
-                String decodedUserData = new String(Base64.decodeBase64(userData.getBytes()), "UTF-8");
-                LOG.debug("UserData: " + decodedUserData);
-                final CloudInstanceUserData cloudUserData = CloudInstanceUserData.deserialize(decodedUserData);
+                final CloudInstanceUserData cloudUserData = CloudInstanceUserData.deserialize(userData);
                 if (cloudUserData != null) {
-
-                    String serverAddress = cloudUserData.getServerAddress();
-                    agentConfiguration.setServerUrl(serverAddress);
-
                     final Map<String, String> customParameters = cloudUserData.getCustomAgentConfigurationParameters();
                     for (Map.Entry<String, String> entry : customParameters.entrySet()) {
                         agentConfiguration.addConfigurationParameter(entry.getKey(), entry.getValue());
@@ -71,7 +65,7 @@ public class OpenstackAgentProperties extends AgentLifeCycleAdapter {
                 }
             }
         } catch (Exception e) {
-            LOG.debug("It seems build-agent launched at non-Openstack instance.");
+            LOG.info("It seems build-agent launched at non-Openstack instance.");
             LOG.error(e.getMessage());
         }
     }
@@ -80,10 +74,11 @@ public class OpenstackAgentProperties extends AgentLifeCycleAdapter {
         String data = "";
         InputStream in = new URL(sURL).openStream();
         try {
-            data = IOUtils.toString(in);
+            data = IOUtils.toString(in, StandardCharsets.UTF_8.name());
         } finally {
             IOUtils.closeQuietly(in);
         }
+
         return data.trim();
     }
 
